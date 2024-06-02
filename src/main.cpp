fa1b5406272af781a8793b6cdce311a76c801e04
Gregor Siefert <gregor.siefert@gmx.de>
2024-06-02 09:54:43 +0200

    Blinken durch Tastereingabe

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,16 @@
 #include <Arduino.h>
 
 void setup(){ 
-// Initialisiert Pin 14 (LED1) als output
+// Initialisiert Pin 14 (LED1) als output und die beiden Taster
 pinMode(14,OUTPUT);
+pinMode(0,INPUT);
+pinMode(1,INPUT);
 }
 
-//LED1 blinkt im Zweisekundentakt
+//LED1 schaltet ein mit Taster 1 und aus mit Taster 2
 void loop(){
 digitalWrite(14,HIGH);
-delay(1000);
+while (digitalRead(1)==0){};
 digitalWrite(14,LOW);
-delay(1000);
+while (digitalRead(0)==0){};
 }
