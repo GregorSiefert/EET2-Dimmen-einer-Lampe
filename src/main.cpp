f471ef7ad52ee2ea6e22529e83aaf8ff0df80d28
Gregor Siefert <gregor.siefert@gmx.de>
2024-06-02 10:50:04 +0200

    Test Spannungsnulldruchgang

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,27 @@
 #include <Arduino.h>
 
+int blink_abstand = 1;  //in sekunden
+int frequenz = 50;
+int needed_count = blink_abstand*frequenz;
+int counter = 0;
+int gateimpuls_micro = 10;
+
 void setup(){ 
 // Initialisiert Pin 14 (LED1) als output
-pinMode(14,OUTPUT);
+pinMode(3,OUTPUT);
+pinMode(6,INPUT);
 }
 
 //LED1 blinkt im Zweisekundentakt
 void loop(){
-digitalWrite(14,HIGH);
-delay(1000);
-digitalWrite(14,LOW);
-delay(1000);
+  	if (digitalRead(6)==1)
+    {
+      counter++;
+      if (counter%needed_count==0) //Triac für eine Halbschwingung anschalten
+      {
+        digitalWrite(3,HIGH);
+        delayMicroseconds(gateimpuls_micro);
+        digitalWrite(3,LOW);
+      }   
+    }    
 }
