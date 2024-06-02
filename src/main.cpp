1709c1a29b6691375998606a8a2ebf236e0627ba
Gregor Siefert <gregor.siefert@gmx.de>
2024-06-02 12:46:25 +0200

    min und max Zündwinkel

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,15 @@ float zuend_winkel = 30;    //in Grad
 int gateimpuls_micro = 10;
 int bool_spnd = 0;  //Bool zum merken, ob Triag schon angeschaltet wurde in dieser Halbschwingung
 
+//Max-Min Berechnung des Triacs
+int einraststrom_mA = 30;
+int haltestrom_mA = 20;
+int U_eff = 230;
+int R_L = 1000;
+
+float zuend_winkel_min = round(asin((einraststrom_mA*R_L)/(sqrt(2)*U_eff))+0.5);
+float zuend_winkel_max = 180 - round(asin((haltestrom_mA*R_L)/(sqrt(2)*U_eff))+0.5);
+
 void setup(){ 
 pinMode(3,OUTPUT);
 pinMode(6,INPUT);
@@ -28,8 +37,16 @@ void loop(){
   if (digitalRead(1)==1)        //Dunkler
   {
     zuend_winkel++;
+    if (zuend_winkel>zuend_winkel_max)
+    {
+      zuend_winkel = zuend_winkel_max;
+    }
   }else if (digitalRead(0)==1)  //Heller
   {
     zuend_winkel--;
+    if (zuend_winkel<zuend_winkel_min)
+    {
+      zuend_winkel = zuend_winkel_min;
+    }
   }
 }
